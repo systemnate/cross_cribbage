c2ec64dae3f7fb363c92254a74e7fb9ea9a3d5fc
Nate Dalo <ndalo@hey.com>
2026-04-07 17:19:05 -0500

    fix: clean up scaffold leftovers, add React mount stub, fix cors methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,13 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch("ALLOWED_ORIGINS", "localhost:3036,localhost:5173").split(",")
-    resource "/api/*", headers: :any, methods: %i[get post options]
-    resource "/cable",  headers: :any, methods: %i[get]
+    resource "/api/*",
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
+
+  allow do
+    origins "*"
+    resource "/cable", headers: :any, methods: %i[get]
   end
 end
