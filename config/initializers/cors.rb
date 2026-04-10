d9bcd77df6766c3f3afc7103cda020dfc2623a46
Nate Dalo <ndalo@hey.com>
2026-04-09 19:42:17 -0500

    security: restrict CORS origins for /cable, add credentials: true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch("ALLOWED_ORIGINS", "localhost:3036,localhost:5173").split(",")
-    resource "/api/*",
-             headers: :any,
-             methods: %i[get post put patch delete options head]
-  end
 
-  allow do
-    origins "*"
-    resource "/cable", headers: :any, methods: %i[get]
+    resource "/api/*",
+             headers:     :any,
+             methods:     %i[get post put patch delete options head],
+             credentials: true
+
+    resource "/cable",
+             headers:     :any,
+             methods:     %i[get],
+             credentials: true
   end
 end
